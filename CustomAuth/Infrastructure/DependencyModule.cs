cb2aaae08f304a8f81e22ba55f8b86a085a8c319
Meshonim <ilya.trukhanovich@gmail.com>
2017-12-09 21:13:15 +0300

    Initial commit

---
+++ CustomAuth/Infrastructure/DependencyModule.cs
@@ -0,0 +1,17 @@
+﻿
+using DalToWeb;
+using DalToWeb.Interfacies;
+using DalToWeb.Repositories;
+using Ninject.Modules;
+
+namespace CW.Infrastructure
+{
+    public class DependencyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserRepository>().To<UserRepository>();
+            Bind<IRoleRepository>().To<RoleRepository>();
+        }
+    }
+}
---
+++ DalToWeb/Interfacies/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DalToWeb.Repositories;
+
+namespace DalToWeb.Interfacies
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAllUsers();
+        bool CreateUser(User user);
+        User GetUserByEmail(string email);
+        bool UpdateUser(User user);
+        bool RemoveUser(int id);
+    }
+}
